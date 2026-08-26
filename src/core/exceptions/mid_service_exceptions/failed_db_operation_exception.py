1c6c8484ead07f45edacded574b67c3f7038dc6e
Saptarshi Ghosh <saptarshi1897@gmail.com>
2026-08-26 19:03:38 +0530

    Training/Testing dataset for Landslide prediction model for NE.

--- src/core/exceptions/mid_service_exceptions/failed_db_operation_exception.py
+++
@@ -1,7 +0,0 @@
-from src.core.exceptions.mid_service_exceptions.mid_service_exceptions import MidServiceExceptions
-'''Failing to write/read to DB after multiple retries, just say the client ,
-failed to load data. try again later.'''
-
-class FailedDBOperationException(MidServiceExceptions):
-    def __init__(self, *args):
-        super().__init__(*args)

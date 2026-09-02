42e0b71f32316db4f55bac841e28ce6119c03289
Saptarshi Ghosh <saptarshi1897@gmail.com>
2026-09-02 14:55:07 +0530

    Made the priority engine responsible for risk assessment of a region based on human exposure, infrastructure,real time traffic, critical lifelines, agricultural land etc.

--- src/core/exceptions/mid_service_exceptions/sms_send_exception.py
+++
@@ -1,6 +0,0 @@
-from src.core.exceptions.mid_service_exceptions.mid_service_exceptions import MidServiceExceptions
-'''If we are unable to send an sms after multiple retries,
-server handles that knwoledge accordingly'''
-class SmsSendException(MidServiceExceptions):
-    def __int__(self):
-        pass
